ecba545f057cede069fed636c6a22764aa501bc9
Michael Chen <mc.michaelchen.us@gmail.com>
2015-10-18 22:54:53 -0700

    Updated for dataMAC

--- static/js/morris-data.js
+++ static/js/morris-data.js
@@ -3,59 +3,59 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
+            period: '2015 Q3',
             iphone: 2666,
             ipad: null,
             itouch: 2647
         }, {
-            period: '2010 Q2',
+            period: '2015 Q4',
             iphone: 2778,
             ipad: 2294,
             itouch: 2441
         }, {
-            period: '2010 Q3',
+            period: '2016 Q1',
             iphone: 4912,
             ipad: 1969,
             itouch: 2501
         }, {
-            period: '2010 Q4',
+            period: '2016 Q2',
             iphone: 3767,
             ipad: 3597,
             itouch: 5689
         }, {
-            period: '2011 Q1',
+            period: '2016 Q3',
             iphone: 6810,
             ipad: 1914,
             itouch: 2293
         }, {
-            period: '2011 Q2',
+            period: '2016 Q4',
             iphone: 5670,
             ipad: 4293,
             itouch: 1881
         }, {
-            period: '2011 Q3',
+            period: '2017 Q1',
             iphone: 4820,
             ipad: 3795,
             itouch: 1588
         }, {
-            period: '2011 Q4',
+            period: '2017 Q2',
             iphone: 15073,
             ipad: 5967,
             itouch: 5175
         }, {
-            period: '2012 Q1',
+            period: '2017 Q3',
             iphone: 10687,
             ipad: 4460,
             itouch: 2028
         }, {
-            period: '2012 Q2',
+            period: '2017 Q4',
             iphone: 8432,
             ipad: 5713,
             itouch: 1791
         }],
         xkey: 'period',
         ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        labels: ['Nearby', 'Eat In', 'Takeout'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -109,7 +109,7 @@ $(function() {
         }],
         xkey: 'y',
         ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        labels: ['Eat In', 'Takeout'],
         hideHover: 'auto',
         resize: true
     });
