c9f20fdb9487bcd39b92a102083d87e23d98519c
Michael Chen <mc.michaelchen.us@gmail.com>
2015-10-18 22:01:37 -0700

    Start dashboard template

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Welcome to DataMAC'
+
+@app.route('/index.html')
+@app.route('/analytics')
+def dashboard():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('login.html'), 404
