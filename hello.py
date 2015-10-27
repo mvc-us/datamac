4311ad6b6ccc2766b36bb13f000e4d99c96b964d
Michael Chen <mc.michaelchen.us@gmail.com>
2015-10-26 19:01:31 -0700

    Redirect /

--- hello.py
+++ hello.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Welcome to DataMAC'
-
 @app.route('/index.html')
 @app.route('/analytics')
 def dashboard():
